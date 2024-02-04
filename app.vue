c7d1d09a5afec2662f8aebd8b30c809c2cd972d7
SerhatPolatAmani <SerhatLiving@hotmail.com>
2024-02-04 17:40:24 +0300

    Changed 'product' naming with 'item' on everywhere
     to make it more flexible for different use cases

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen w-screen bg-slate-300">
     <div class="max-w-screen-xl mx-auto px-6 py-12">
       <div v-if="items && items.length > 0" class="grid lg:grid-cols-2 gap-6">
-        <Product
+        <Item
           v-for="item in items"
           :key="item.id"
           :name="item.name ? item.name : '-'"
--- services/api.js
+++ services/api.js
@@ -6,7 +6,7 @@ const api = axios.create({
 
 export const getItems = async () => {
   try {
-    const response = await api.get("/products");
+    const response = await api.get("/items");
     return response.data;
   } catch (error) {
     console.error("Error fetching items:", error);
@@ -16,7 +16,7 @@ export const getItems = async () => {
 
 export const createItem = async (item) => {
   try {
-    const response = await api.post("/products", item);
+    const response = await api.post("/items", item);
     return response.data;
   } catch (error) {
     console.error("Error creating item:", error);
@@ -26,7 +26,7 @@ export const createItem = async (item) => {
 
 export const updateItem = async (id, item) => {
   try {
-    const response = await api.put(`/products/${id}`, item);
+    const response = await api.put(`/items/${id}`, item);
     return response.data;
   } catch (error) {
     console.error("Error updating item:", error);
@@ -36,7 +36,7 @@ export const updateItem = async (id, item) => {
 
 export const deleteItem = async (id) => {
   try {
-    const response = await api.delete(`/products/${id}`);
+    const response = await api.delete(`/items/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error deleting item:", error);
